ffda474a4fc91a5c546e22a158f28f006a77a36c
mbaker <mekenna.drabner@gmail.com>
2024-08-31 11:58:58 -0600

    added projects info array to hold projects. Created with placeholder information

--- React Portfolio/src/components/header.jsx
+++ React Portfolio/src/components/header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" to="/">My Portfolio</NavLink>
+                    <NavLink className="navbar-brand" to="/">Mekenna Baker</NavLink>
+
+                    {/* Hamburger button  to handle navbar on smaller screens */}
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -16,10 +18,13 @@ const Header = () => {
                         aria-expanded="false"
                         aria-label="Toggle navigation"
                     >
+                        {/* Hamburger icon */}
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
+                            
+                            {/*Navbar list items with navlink to navigate to corresponding part of the website when the file path matches  */}
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/" exact>About Me</NavLink>
                             </li>
@@ -27,7 +32,7 @@ const Header = () => {
                                 <NavLink className="nav-link" to="/projects">Projects</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/contact">Contact Me!</NavLink>
+                                <NavLink className="nav-link" to="/contact">Contact Me</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/resume">My Resume</NavLink>
--- React Portfolio/src/components/projects.jsx
+++ React Portfolio/src/components/projects.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
 
+// Create an aray of objects to hold the project data //
+
+const projectsInfo = [
+    {
+        title: 'Project 1',
+        description: 'description of project 1',
+        image: 'image path',
+        deployedLink: 'deployed project link',
+        repoLink: 'github repo link'
+    },
+    {
+        title: 'Project 2',
+        description: 'description of project 2',
+        image: 'image path',
+        deployedLink: 'deployed project link',
+        repoLink: 'github repo link'
+    },
+    {
+        title: 'Project 3',
+        description: 'description of project 3',
+        image: 'image path',
+        deployedLink: 'deployed project link',
+        repoLink: 'github repo link'
+    },
+    {
+        title: 'Project 4',
+        description: 'description of project 4',
+        image: 'image path',
+        deployedLink: 'deployed project link',
+        repoLink: 'github repo link'
+    },
+    {
+        title: 'Project 5',
+        description: 'description of project 5',
+        image: 'image path',
+        deployedLink: 'deployed project link',
+        repoLink: 'github repo link'
+    },
+    {
+        title: 'Project 6',
+        description: 'description of project 6',
+        image: 'image path',
+        deployedLink: 'deployed project link',
+        repoLink: 'github repo link'
+    },
+];
+
 const Projects = () => {
     return (
         <div>
