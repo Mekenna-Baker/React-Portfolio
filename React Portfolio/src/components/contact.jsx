5662112f908934da909f1707965b3408119c88a4
mbaker <mekenna.drabner@gmail.com>
2024-09-01 14:04:52 -0600

    Utilized useState to hold errors and form data. Created functions to validate form fields, and update the form upon input.

--- React Portfolio/src/components/contact.jsx
+++ React Portfolio/src/components/contact.jsx
@@ -1,12 +1,100 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Contact = () => {
+    // Create a state to hold the form data
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+    // Create a state to hold the errors
+    const [errors, setErrors] = useState({})
+
+    // Function to validate email
+    const validateEmail = (email) => {
+        // Finding the index of @ and .
+        const atIndex = email.indexOf('@');
+        const dotIndex = email.lastIndexOf('.');
+
+        // Returning true if the email is valid                                                            
+        return atIndex > 0 && dotIndex > atIndex + 1 && dotIndex < email.length - 1;
+    };
+
+    // Function to validate field(s)
+    const validateField = (e) => {
+        // Checking if input field is empty
+        if (!e.target.value) {
+            // If empty, set the error message
+            setErrors({ ...errors, [e.target.name]: `${e.target.name} is required!` });
+
+            // Checking if the email is valid
+        } else if (e.target.name === 'email' && !validateEmail(e.target.value)) {
+            // If email is not valid, set the error message
+            setErrors({ ...errors, email: 'Please enter a valid email address!' })
+
+            // If information is valid, remove the error message
+        } else {
+            const newErrors = { ...errors };
+            delete newErrors[e.target.name];
+            setErrors(newErrors);
+        }
+    }
+
     return (
-        <div>
-            <h1> Contact Me</h1>
-            <p>This is the contact me section</p>
-        </div>
+        <section className="container mt-5">
+            <h2>Contact Me</h2>
+            <form>
+                {/* Name input */}
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        className="form-control"
+                        value={formData.name}
+                        // Update formData state on input change
+                        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                        // Validates when cursor leaves the input field
+                        onBlur={validateField}
+                    />
+                    {/* Display error message if name is empty */}
+                    {errors.name && <p className="text-danger">{errors.name}</p>}
+                </div>
+
+                {/* Email input */}
+                <div className="mb-3">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        className="form-control"
+                        value={formData.email}
+                        // Update formData state on input change
+                        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                        // Validates when cursor leaves the input field
+                        onBlur={validateField}
+                    />
+                    {/* Display error message if email is empty or invalid */}
+                    {errors.email && <p className="text-danger">{errors.email}</p>}
+                </div>
+
+                {/* Message input */}
+                <div className="mb-3">
+                    <textarea
+                        type="text"
+                        name="message"
+                        placeholder="Enter your message here"
+                        className="form-control"
+                        value={formData.message}
+                        // Update formData state on input change
+                        onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+                        // Validates when cursor leaves the input field
+                        onBlur={validateField}
+                    />
+                    {/* Display error message if the message is invalid */}
+                    {errors.message && <p className="text-danger">{errors.message}</p>}
+                </div>
+
+                {/* Submit button */}
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </section>
     );
 };
 
--- React Portfolio/src/components/projects.jsx
+++ React Portfolio/src/components/projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Create an aray of objects to hold the project data //
+// Create an array of objects to hold the project data //
 
 const projectsInfo = [
     {
