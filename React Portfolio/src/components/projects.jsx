f2ba4e6a84f2370e3384ff0bc25cc51bb215b05c
mbaker <mekenna.drabner@gmail.com>
2024-09-02 13:16:09 -0600

    added more css to header and footer css files

--- React Portfolio/src/components/projects.jsx
+++ React Portfolio/src/components/projects.jsx
@@ -62,19 +62,21 @@ const Projects = () => {
                     <div className="col-md-4 mb-4" key={index}>
                         <div className="card">
                             <img src={project.image} className="card-img-top" alt={project.title} />
-                            <div className="card-body">
 
+                            <div className="card-body">
                                 <h5 className="card-title">{project.title}</h5>
                                 <p className="card-text">{project.description}</p>
 
                                 {/* The projects' deployed links */}
-                                <a href={project.deployedLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
-                                    Deployed Project
-                                </a>
-                                {/* The projects' repository links */}
-                                <a href={project.repoLink} className="btn btn-secondary ml-2" target="_blank" rel="noopener noreferrer">
-                                    Project Repository
-                                </a>
+                                <div className="button-container">
+                                    <a href={project.deployedLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                                        Deployed Project
+                                    </a>
+                                    {/* The projects' repository links */}
+                                    <a href={project.repoLink} className="btn btn-secondary ml-2" target="_blank" rel="noopener noreferrer">
+                                        Project Repository
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
