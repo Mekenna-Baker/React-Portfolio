a37de112a3316193215f2e437452dfb08f89ae65
mbaker <mekenna.drabner@gmail.com>
2024-11-28 13:11:56 -0700

    edited resume jsx and resume css to add skills to resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import '../css/Resume.css';
 
-const Resume = () => {
-    return (
-
-        <div className="resume-container">
-
-        <h2>If you'd like to learn more...</h2> 
-        <p>feel free to download my resume!</p> 
-
-        
-        <div className="resume-download btn btn-primary">
-
-            <a class="resume-button" href="public/MekennaBaker_CV.docx"
-            download="MekennaBaker_CV.docx">Download My Resume
-            </a>
-
+function Resume() {
+  return (
+    <div className="resume-container">
+      <h1>My Skills</h1>
+      <div className="skills-boxes">
+        <div className="skill-box">
+          <h2>Front End</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
         </div>
+        <div className="skill-box">
+          <h2>Back End</h2>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MongoDB</li>
+            <li>PostgreSQL</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+        <div className="skill-box">
+          <h2>Tools & Platforms</h2>
+          <ul>
+            <li>Git/GitHub</li>
+            <li>VS Code</li>
+            <li>Render</li>
+            <li>Indsomia</li>
+          </ul>
+        </div>
+      </div>
+      <div className="resume-download">
+        <button className="download-btn">Download My Resume</button>
+      </div>
     </div>
-    );
-};
+  );
+}
 
 export default Resume;
