b1930b3814f2317ec1627a2ec2864f5c12354d97
mbaker <mekenna.drabner@gmail.com>
2024-11-27 20:59:00 -0700

    updated anchor tags within the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ import { faGithub, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-i
 const Footer = () => {
     return (
         <footer >
-            <a href="https://github.com/Mekenna-Baker" target="_blank">
+            <a href="https://github.com/Mekenna-Baker" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faGithub} /> </a>
 
-            <a href="https://www.linkedin.com/in/mekenna--baker/" target="_blank">
+            <a href="https://www.linkedin.com/in/mekenna--baker/" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faLinkedin} />  </a>
 
-            <a href="https://www.facebook.com/mekenna.drabner.04/" target="_blank">
+            <a href="https://www.facebook.com/mekenna.drabner.04/" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faFacebook} />
             </a>
 
